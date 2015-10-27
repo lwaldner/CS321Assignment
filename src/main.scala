c06888783822d1ce72ff76c3263afe73e043e1ab
Luke Waldner <lwaldne1@ithaca.edu>
2015-10-26 18:08:53 -0700

    setting more stuff up

--- src/main.scala
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lwald_000 on 10/26/2015.
- */
-class main {
-
-}
---
+++ src/objects/main.scala
@@ -0,0 +1,12 @@
+package objects
+
+/**
+ * Created by lwald_000 on 10/26/2015.
+ */
+object main {
+
+  def main (args: Array[String]): Unit = {
+    println("Hello")
+  }
+
+}
