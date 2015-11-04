f312c7ce425f1db0b9d28dfa0744df74dd1e1a55
Chris <chrisv.perez@gmail.com>
2015-11-04 15:59:35 -0500

    refactoring and player initialization

--- src/objects/main.scala
+++ src/objects/main.scala
@@ -1,5 +1,7 @@
 package objects
 
+import classes.Player
+
 /**
  * Created by lwald_000 on 10/26/2015.
  */
@@ -7,6 +9,11 @@ object main {
 
   def main (args: Array[String]): Unit = {
     println("Hello")
+    val numPlayers = 3
+    val players = List[Player]()
+    for (i <- 1 to numPlayers) {
+      players :+ new Player
+    }
   }
 
 }
